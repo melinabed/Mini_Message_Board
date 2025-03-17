8e87d3bafbf63d42b672cdd6cf229c0c2b2cd712
melinabed <141087878+melinabed@users.noreply.github.com>
2025-03-17 10:24:16 -1000

    updated form style

--- public/frontend/script.js
+++ public/frontend/script.js
@@ -6,7 +6,7 @@ const themeIcon = document.querySelector(".icon");
 themeButton.addEventListener("click", () => {
   if (themeIcon.src.includes("/icons/moon.svg")) {
     themeIcon.src = "/icons/sun.svg";
-    body.style.backgroundColor = "black";
+    body.style.backgroundColor = "#24292e";
   } else {
     themeIcon.src = "/icons/moon.svg";
     body.style.backgroundColor = "#f6f8fa";
