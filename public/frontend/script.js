2c25eeb96c79e2d4b551974e925be7c80a81f04e
melinabed <141087878+melinabed@users.noreply.github.com>
2025-03-17 10:37:28 -1000

    added more css for element styles changing for theme switch

--- public/frontend/script.js
+++ public/frontend/script.js
@@ -2,13 +2,22 @@ const body = document.querySelector("body");
 
 const themeButton = document.querySelector("#theme-btn");
 const themeIcon = document.querySelector(".icon");
+const messageItems = document.querySelectorAll("li");
 
 themeButton.addEventListener("click", () => {
   if (themeIcon.src.includes("/icons/moon.svg")) {
     themeIcon.src = "/icons/sun.svg";
     body.style.backgroundColor = "#24292e";
+    messageItems.forEach((item) => {
+      item.classList.add("dark-mode-message-item");
+      item.classList.remove("light-mode-message-item");
+    });
   } else {
     themeIcon.src = "/icons/moon.svg";
     body.style.backgroundColor = "#f6f8fa";
+    messageItems.forEach((item) => {
+      item.classList.remove("dark-mode-message-item");
+      item.classList.add("light-mode-message-item");
+    });
   }
 });
